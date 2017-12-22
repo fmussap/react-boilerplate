d72d832dfaec1dfd5fc04b5ec1176598a4aa80b5
fmussap <fabiomussap@yahoo.com>
2017-12-22 10:51:48 +0000

    update login

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 const DashboardPage = () => (
   <div>
     Dashboard page content
   </div>
-);
+)
 
-export default DashboardPage;
+export default DashboardPage
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase'
 
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -7,11 +7,11 @@ const config = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
+}
 
-firebase.initializeApp(config);
+firebase.initializeApp(config)
 
-const database = firebase.database();
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const database = firebase.database()
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
 
-export { firebase, googleAuthProvider, database as default };
+export { firebase, googleAuthProvider, database as default }
